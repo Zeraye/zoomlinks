9decca6fc6edac2d30294f71ba056f0dc4cd6b7f
Zeraye <jakubrudnik02@gmai.com>
2020-12-25 17:52:07 +0100

    staticfiles repair

--- zoomlinks/settings.py
+++ zoomlinks/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (Path.joinpath(BASE_DIR, 'static'))
+STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'static')]
