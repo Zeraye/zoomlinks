98f31ae325c27491d8146b07b58b819dc9c9ef1a
Zeraye <jakubrudnik02@gmai.com>
2021-02-18 12:14:17 +0100

    hours shown in myclass

--- myclass/views.py
+++ myclass/views.py
@@ -8,11 +8,18 @@ def myclass(request):
     lessons_all = lessons(request, request.user.class_name)
     today_day_name = lessons_all[3]
 
-    try: lessons_today = list(set(lessons_all[1][today_day_name]))
+    try: lessons_today = list((lessons_all[1][today_day_name]))
     except:
         lessons_today = []
         for lesson_name in lessons_all[2]:
-            lessons_today.append(lesson_name)
+            lessons_today.append((lesson_name, '11:30-12:30'))
+
+    print(lessons_today)
+
+    lessons_today = list(map(lambda lesson, time: (lesson, time), lessons_today,
+        ['8:00-8:45', '8:50-9:35', '9:45-10:30', '10:40-11:25', '11:30-12:15', '12:45-13:30', '13:35-14:20', '14:25-15:10', ]))
+
+    print(lessons_today)
 
     context = {'current_lesson': lessons_all[0],
                'lessons_today': lessons_today,
